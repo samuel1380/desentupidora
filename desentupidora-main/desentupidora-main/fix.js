583e60fcbadd63046082e0843049d541be626615
samuel1380 <samuelprincipal345@gmail.com>
2026-05-28 16:26:34 -0300

    Delete desentupidora-main/desentupidora-main directory

--- desentupidora-main/desentupidora-main/fix.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/elementor-invisible/g, '');
-html = html.replace(/data-lazyloaded="1"\s+src="data:image[^"]+"/g, '');
-html = html.replace(/data-src="[^"]+\/([^"\/]+)"/g, 'src="images/$1"');
-fs.writeFileSync('index.html', html, 'utf8');
